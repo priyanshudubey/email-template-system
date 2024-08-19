a665d4528862ecf8f9d5b97b6ea2a2c278e5ac0e
priyanshudubey <priyanshu0dubey@gmail.com>
2024-08-19 22:09:51 +0100

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Browse from "./components/Browse";
+
+function App() {
+  return (
+    <div className="App">
+      <Browse />
+    </div>
+  );
+}
+
+export default App;
